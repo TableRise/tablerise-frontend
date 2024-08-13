3645a896a0e0d8829ecf77bccab5888835270138
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-08-13 09:48:26 -0300

    feat: Added the functionality to open/close the FAQ item

--- src/app/components/FAQItem.tsx
+++ src/app/components/FAQItem.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import Image from 'next/image';
-import more from '../../../assets/icons/nav/add-16.svg'
+import more from '../../../assets/icons/nav/add-16.svg';
 
 export default function FAQItem() {
+    const [details, setDetails] = useState<boolean>(false);
+
+    function handleFAQItem(): void {
+        setDetails(!details);
+    }
+
     return (
-        <li className='bg-[#292929] flex p-4 justify-between rounded-xl'>
-            <h2 className='font-L-semibold'>Title</h2>
-            <button className='border border-[#bdbdbd] rounded-[4px] w-6 h-6 flex items-center justify-center'>
-                <Image
-                    src={ more }
-                    alt='More details'
-                />
-            </button>
+        <li className='w-full bg-[#292929] p-4 rounded-xl'>
+            <div className='w-full flex justify-between'>
+                <h2 className='font-L-semibold'>Title</h2>
+                <button
+                    className='border border-[#bdbdbd] rounded-[4px] w-6 h-6 flex items-center justify-center'
+                    onClick={ handleFAQItem }
+                >
+                    <Image
+                        src={ more }
+                        alt='More details'
+                    />
+                </button>
+            </div>
+            <p hidden={ !details }>Answer</p>
         </li>
     )
 
