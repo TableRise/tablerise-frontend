7a9fcaab8f6484322847660b0aabd97ff3e0a84e
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-08-13 14:47:09 -0300

    feat: When the question is expanded, the toggle button changes

--- src/app/components/ExpandBtnn.tsx
+++ src/app/components/ExpandBtnn.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
-import more from '../../../assets/icons/nav/add-16.svg';
+import expand from '../../../assets/icons/nav/add-16.svg';
+import colapse from '../../../assets/icons/nav/reduce.svg';
 
 type ExpandButtonProps = {
-    handler: () => void
+    handler: () => void,
+    expandState: boolean
 }
 
-export default function ExpandBtn({ handler }: ExpandButtonProps) {
+export default function ExpandBtn({ handler, expandState }: ExpandButtonProps) {
     return (
         <button
         className='border border-[#bdbdbd] rounded-[4px] w-6 h-6 flex items-center justify-center'
         onClick={ handler }
     >
         <Image
-            src={ more }
+            src={ expandState ? colapse : expand }
             alt='More details'
         />
     </button>
--- src/app/components/FAQItem.tsx
+++ src/app/components/FAQItem.tsx
@@ -3,19 +3,19 @@ import React, { useState } from 'react';
 import ExpandBtn from './ExpandBtnn';
 
 export default function FAQItem() {
-    const [details, setDetails] = useState<boolean>(false);
+    const [expand, setExpand] = useState<boolean>(false);
 
     function handleFAQItem(): void {
-        setDetails(!details);
+        setExpand(!expand);
     }
 
     return (
         <li className='w-full bg-[#292929] p-4 rounded-xl flex flex-col gap-3'>
             <div className='w-full flex justify-between'>
                 <h2 className='font-L-semibold'>Title</h2>
-                <ExpandBtn handler={ handleFAQItem } />
+                <ExpandBtn handler={ handleFAQItem } expandState={ expand } />
             </div>
-            <p hidden={ !details }>Answer</p>
+            <p hidden={ !expand }>Answer</p>
         </li>
     )
 
