4d5f4d13eae35c8be7845ec1db72e4861357261c
Amanda Portela <mandaptela@gmail.com>
2024-08-14 21:46:33 -0300

    create sociallogin component initial structure

---
+++ src/app/components/SocialLogin.tsx
@@ -0,0 +1,8 @@
+export default function SocialLogin() {
+    return (
+        <main>
+            <button type='button'>Discord</button> {/* login pelo disc */}
+            <button type='button'>Google</button> {/* login pelo google */}
+        </main>
+    );
+}
