df0f31434420c555f1e885a1ac629141a566b9f5
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2024-08-16 22:57:13 -0300

    feat: Final adjustments before FAQ

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,4 +1,5 @@
 import '@/components/home/styles/Banner.css';
+import Link from 'next/link';
 
 export default function Banner(): JSX.Element {
     return (
@@ -7,14 +8,19 @@ export default function Banner(): JSX.Element {
                 <p className="font-XXL-bold">
                     Bem vindos ao TableRise!
                     <br />
-                    Sua ferramenta favorita de RPG de Mesa
+                    Sua ferramenta favorita para
+                    <br /> jogar RPG de Mesa
                 </p>
                 <p className="font-M-regular">
                     Crie e jogue suas histórias de maneira
                     <br />
                     prática e descomplicada
                 </p>
-                <button className="button-white-default font-S-bold">Registrar-se</button>
+                <Link href="/register">
+                    <button className="button-white-default font-S-bold">
+                        Registrar-se
+                    </button>
+                </Link>
             </div>
             <div className="main-image" />
         </section>
