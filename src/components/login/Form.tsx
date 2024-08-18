feb8f037a8b40f6fda45dc465574000ca86c4a6d
Amanda Portela <mandaptela@gmail.com>
2024-08-17 23:23:34 -0300

    feat: adding functions basic informations

--- src/components/login/Form.tsx
+++ src/components/login/Form.tsx
@@ -4,46 +4,42 @@ import Input from "./input";
 import { input } from "@/types/login/types";
 
 export default function Form() {
-    const MAX_LENGTH_EMAIL = 30;
-    const MAX_LENGTH_PASS = 50;
-
-    const [userEmail, setEmail] = useState('');
-    const [userPassword, setPassword] = useState('');
     const [showPass, setShowPass] = useState(false);
     const [loading, setLoading] = useState(false);
-
-    const userData: Object = { userEmail, userPassword };
 
     const emailProps: input = {
         type: "email",
         name: "E-mail",
         placeholder: "Insira o seu e-mail",
-        onChange: setEmail,
         id: "emailInput",
-        maxLength: MAX_LENGTH_EMAIL,
     }
 
     const passProps: input = {
         type: "password",
         name: "password",
         placeholder: "Insira a sua senha",
-        onChange: setPassword,
         id: "passwordInput",
-        maxLength: MAX_LENGTH_PASS,
+    }
+
+    function handleLogin(data: any) {
+        //PEGAR DADOS DO CONTEXT
+        "response = api.post({data}"
+        "if response nao for ok return (erro)"
     }
 
     return (
         <main>
-            <div>
+            <div id="form-title">
                 <p>Entrar</p>
                 <span>Não possui uma conta? <a href="#">Criar conta!</a></span>
                 {/* levar p pag de cadastro */}
             </div>
-            <form>
+            <form onSubmit={ (event) => { event.preventDefault(), handleLogin("data") } }>
                 <Input props={emailProps}/> 
                 <Input props={ passProps } />
                 <a href="#">Esqueceu a senha?</a> {/* levar p reset de senha */}
-                <button type='button'>Entrar</button>
+                <button type='button' onClick={(event) => handleLogin(event.target)
+                }>Entrar</button>
                 {/* salvar user e token no localstorage e redirecionar p home */}
             </form>
         </main>
