8fdb384ac36d1b2f5948b04a11d7b98313bcb23d
Cesar Holanda <cesar.holanda@lexartlabs.xyz>
2024-08-20 13:04:10 -0300

    feat: create errorHandler

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,14 @@
+export default function errorHandler(error: string) {
+    switch (error) {
+        case 'username in use':
+            return { inputId: 'username', message: 'Esse usuário já foi cadastrado' };
+        case 'email in use':
+            return { inputId: 'email', message: 'Esse e-mail já foi cadastrado' };
+        case 'password not match':
+            return { inputId: 'password', message: 'Esses campos não coincidem' };
+        case 'terms not accepted':
+            return { inputId: 'checkbox', message: 'Você precisa aceitar os termos' };
+        default:
+            return { inputId: 'login', message: 'Erro ao fazer login' };
+    }
+}
