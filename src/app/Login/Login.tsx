2ee7aa6198718c06ba53427f01a376dfaca34119
Amanda Portela <mandaptela@gmail.com>
2024-08-20 21:45:44 -0300

    ref: header off + temporary organization

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,18 +1,16 @@
-import Form from '@/components/login/Form';
+import LoginForm from '@/components/login/LoginForm';
 import SocialLogin from '@/components/login/SocialLogin';
 import Header from '@/components/Header';
 
 export default function Login() {
-return (
-    <section>
-        <Header />
-        <main>
-            <Form />
+    return (
+        <div>
+            <LoginForm />
             <div>
                 <hr />
                 <span>ou continue com</span>
+                <SocialLogin />
             </div>
-            <SocialLogin />
-        </main>
-    </section>
-)}
+        </div>
+    )
+}
