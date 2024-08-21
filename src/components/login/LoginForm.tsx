878b82e8594aa54d3fcbfacccc97bf8f4755cde3
Amanda Portela <mandaptela@gmail.com>
2024-08-20 21:40:54 -0300

    ref: renamed and turned into main component to handle data

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -2,16 +2,20 @@
 import { useState } from "react";
 import Input from "./input";
 import { input } from "@/types/login/types";
+import Link from "next/link";
 
 export default function Form() {
     const [showPass, setShowPass] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [userEmail, setEmail] = useState('');
+    const [userPassword, setPassword] = useState('');
 
     const emailProps: input = {
         type: "email",
         name: "E-mail",
         placeholder: "Insira o seu e-mail",
         id: "emailInput",
+        onChange: setEmail,
     }
 
     const passProps: input = {
@@ -19,9 +23,28 @@ export default function Form() {
         name: "password",
         placeholder: "Insira a sua senha",
         id: "passwordInput",
+        onChange: setPassword,
     }
 
+/*     function validateInput(value: string, type: string) {
+        const MIN_LENGTH_PASS = 6;
+        if(type === 'email') { 
+            const validHas: RegExp =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+            if(validHas.test(value)) {
+                console.log('OK');
+                
+            }
+            console.log("email invalido");
+            
+        }
+        else if (value.length < MIN_LENGTH_PASS) {
+            console.log(`campo vermelho enquanto menos de 6 caracteres` );
+        }
+    } */
+
+
     function handleLogin(data: any) {
+        console.log(data);
         //PEGAR DADOS DO CONTEXT
         "response = api.post({data}"
         "if response nao for ok return (erro)"
@@ -31,12 +54,11 @@ export default function Form() {
         <main>
             <div id="form-title">
                 <p>Entrar</p>
-                <span>Não possui uma conta? <a href="#">Criar conta!</a></span>
-                {/* levar p pag de cadastro */}
+                <span>Não possui uma conta? <Link href={'/register'} />Criar conta!</span>
             </div>
             <form onSubmit={ (event) => { event.preventDefault(), handleLogin("data") } }>
-                <Input props={emailProps}/> 
-                <Input props={ passProps } />
+                <Input props={emailProps} onChange={setEmail}/> 
+                <Input props={ passProps } onChange={setPassword} />
                 <a href="#">Esqueceu a senha?</a> {/* levar p reset de senha */}
                 <button type='button' onClick={(event) => handleLogin(event.target)
                 }>Entrar</button>
