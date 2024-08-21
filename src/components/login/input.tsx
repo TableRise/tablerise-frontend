6080285b8f179c7ea83021c7ccdcabe1e0aa278d
Amanda Portela <mandaptela@gmail.com>
2024-08-20 21:43:06 -0300

    ref:  component now only for receiving data from login's inputs

--- src/components/login/input.tsx
+++ src/components/login/input.tsx
@@ -1,33 +1,6 @@
-import { useRef, useState } from "react";
-
 export default function Input({ props }: any) {
-    //FAZER USANDO CONTEXT
-    const [userEmail, setEmail] = useState('');
-    const [userPassword, setPassword] = useState('');
-
     const MAX_LENGTH_EMAIL = 30;
     const MAX_LENGTH_PASS = 50;
-    const MIN_LENGTH_PASS = 6;
-
-    function validateInput(value: string, type: string) {
-        if(type === 'email') { 
-            const validHas: RegExp =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-            validHas.test(value) ? console.log("ok") : console.log("email invalido");
-            
-        }
-        else if (value.length < MIN_LENGTH_PASS) {
-            console.log(`campo vermelho enquanto menos de 6 caracteres` );
-        }
-    }
-
-    //pegando email e senha
-    function handleInput(value: string, type: string) {        
-        validateInput(value, props.type);
-        if(type === 'email') {        
-            setEmail(value);
-        }
-        setPassword(value);
-    }
 
     return (
         <div>
@@ -37,7 +10,7 @@ export default function Input({ props }: any) {
                     type={props.type}
                     maxLength={props.type === "email" ? MAX_LENGTH_EMAIL : MAX_LENGTH_PASS }
                     placeholder={props.placeholder}
-                    onChange={({target}) => handleInput(target.value, props.type)}
+                    onChange={({ target }) => props.onChange(target.value)}
                 />
             </label>
         </div>
