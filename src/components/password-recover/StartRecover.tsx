c047e9c4c05f6848ea700c757fdc4570ae565775
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-22 15:22:18 -0300

    feat: create StartRecover

---
+++ src/components/password-recover/StartRecover.tsx
@@ -0,0 +1,17 @@
+import Form from "./forms/Form";
+import FormStartRecover from "./forms/form-start-recover";
+
+export default function StartRecover() {
+    return (
+        <div>
+            <Form.Title>
+                Recuperar senha
+            </Form.Title>
+            <Form.Description>
+                Insira o e-mail associado à sua conta e enviaremos um e-mail com instruções para redefinir sua senha.
+            </Form.Description>
+            
+            <FormStartRecover />
+        </div>
+    )
+}
