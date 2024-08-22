cebfa848c33cbc2f023a5b3f22d92a57496de039
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-22 17:16:53 -0300

    fix: fix path in schema

--- src/components/password-recover/forms/schemas/form-new-password-schema.ts
+++ src/components/password-recover/forms/schemas/form-new-password-schema.ts
@@ -5,7 +5,7 @@ export const newPasswordSchema = z.object({
     confirmPassword: z.string().min(1, { message: "O campo não pode estar vazio." }),
 }).refine((data) => data.confirmPassword === data.newPassword, {
     message: "Esses campos não coincidem.",
-    path: ["oldPassword"], // Define o campo onde o erro será exibido
+    path: ["confirmPassword"], // Define o campo onde o erro será exibido
   });
 
 export type NewPasswordSchema = z.infer<typeof newPasswordSchema>
