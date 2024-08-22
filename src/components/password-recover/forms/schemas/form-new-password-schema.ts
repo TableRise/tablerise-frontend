88e6068bfbc77acbc56d92d92f987ddce969b82f
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-22 17:12:38 -0300

    feat: create schema for new password form

---
+++ src/components/password-recover/forms/schemas/form-new-password-schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const newPasswordSchema = z.object({
+    newPassword: z.string().min(1, { message: "O campo não pode estar vazio." }),
+    confirmPassword: z.string().min(1, { message: "O campo não pode estar vazio." }),
+}).refine((data) => data.confirmPassword === data.newPassword, {
+    message: "Esses campos não coincidem.",
+    path: ["oldPassword"], // Define o campo onde o erro será exibido
+  });
+
+export type NewPasswordSchema = z.infer<typeof newPasswordSchema>
