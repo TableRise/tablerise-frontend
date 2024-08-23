fed9c79b072e13ebb64e895b20a7f668f9b1fd94
Cesar Holanda <cesar.holanda@lexartlabs.xyz>
2024-08-23 18:03:16 -0300

    feat: add wrapper

---
+++ src/server/wrapper.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const apiCall = async (
+    baseUrl: string | undefined,
+    endpoint: string,
+    method: string = 'GET',
+    data?: any
+) => {
+    try {
+        const response = await axios({
+            method,
+            url: `${baseUrl}/${endpoint}`,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data,
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
