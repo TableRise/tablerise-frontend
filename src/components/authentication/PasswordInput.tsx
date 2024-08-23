cd2a4ea67f58b598c913ed9bd07e450e41b5bf52
Cesar Holanda <cesar.holanda@lexartlabs.xyz>
2024-08-23 17:55:05 -0300

    fix: add visible icon to password input

--- src/components/authentication/PasswordInput.tsx
+++ src/components/authentication/PasswordInput.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@/components/authentication/styles/TextInput.css';
 import { PasswordInputProps } from '@/types/modules/components/authentication/PasswordInput';
+import VisibilityIcon from '../../../assets/icons/sys/visibility.svg';
+import VisibilityOffIcon from '../../../assets/icons/sys/visibility-off.svg';
 
 export default function PasswordInput({
     label,
@@ -11,18 +13,37 @@ export default function PasswordInput({
     errorList,
 }: PasswordInputProps): JSX.Element {
     const hasError = errorList.inputId === errorId;
+    const [visible, setVisible] = useState<boolean>(false);
     return (
         <label className="label">
             <h1 className="label-text font-S-bold">{label}</h1>
-            <input
-                className={` ${
-                    hasError ? 'input-error-light' : 'input-i2-light'
-                } input font-XS-regular focus:input-active-light`}
-                placeholder={placeholder}
-                type="password"
-                onChange={({ target: { value } }) => onChangeState(value)}
-                value={inputValue}
-            />
+            <div className="flex justify-between items-center ">
+                <input
+                    className={` ${
+                        hasError ? 'input-error-light' : 'input-i2-light'
+                    } input font-XS-regular focus:input-active-light`}
+                    placeholder={placeholder}
+                    type={visible ? 'text' : 'password'}
+                    onChange={({ target: { value } }) => onChangeState(value)}
+                    value={inputValue}
+                />
+                <span
+                    className="flex justify-around items-center"
+                    onClick={() => setVisible((prevState) => !prevState)}
+                >
+                    {visible ? (
+                        <VisibilityOffIcon
+                            className="absolute mr-10 bg-colo w-6 h-6"
+                            color="#7c7c7c"
+                        />
+                    ) : (
+                        <VisibilityIcon
+                            className="absolute mr-10 w-6 h-6"
+                            color="#7c7c7c"
+                        />
+                    )}
+                </span>
+            </div>
             {hasError && (
                 <p className="error-message font-XXS-bold">{errorList.message}</p>
             )}
