f1ab53b8dc4a3b36a94dfa2f1aa6e17c35bdbbe5
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-23 15:53:46 -0300

    feat: add svg logo

--- src/app/password-recover/template.tsx
+++ src/app/password-recover/template.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image';
+import logo from '@assets/icons/logo.svg'
 import './page.css';
 
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-full h-screen flex justify-end bg-[#4E69A0]">
+            <div className='flex items-center justify-center w-full h-screen text-white'>
+                <Image src={logo} alt='Logo tablerise' width={500} height={500} />
+            </div>
             <div className="section-container">
                 {children}
             </div>
