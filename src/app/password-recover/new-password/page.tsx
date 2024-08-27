a7385dc5fe9ed0e5c4c18c73e0701001bb605599
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-27 15:00:00 -0300

    feat: create new-password and verify-code page

---
+++ src/app/password-recover/new-password/page.tsx
@@ -0,0 +1,9 @@
+import NewPassword from "@/components/password-recover/NewPassword";
+
+export default function Page() {
+    return (
+        <section>
+            <NewPassword />
+        </section>
+    )
+}
