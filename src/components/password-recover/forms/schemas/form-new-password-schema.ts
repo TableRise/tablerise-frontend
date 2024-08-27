a9cdc4408219438b75b677c80a2f949b52186cd1
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-27 15:10:01 -0300

    feat: add regex for password

--- src/components/password-recover/forms/schemas/form-new-password-schema.ts
+++ src/components/password-recover/forms/schemas/form-new-password-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const newPasswordSchema = z.object({
-    newPassword: z.string().min(1, { message: "O campo não pode estar vazio." }),
+    newPassword: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/, { message: "Formato invalido de senha" }),
     confirmPassword: z.string().min(1, { message: "O campo não pode estar vazio." }),
 }).refine((data) => data.confirmPassword === data.newPassword, {
     message: "Esses campos não coincidem.",
