abbb6adfa1c0f3471fcc6f802660d6130be70ba8
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-27 13:44:50 -0300

    feat: implements form sendEmail

--- src/components/password-recover/forms/form-start-recover.tsx
+++ src/components/password-recover/forms/form-start-recover.tsx
@@ -3,45 +3,66 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from '@hookform/resolvers/zod'
 import { emailSchema, EmailSchema } from "./schemas/form-start-recover-schema";
 import Form from "./Form";
+import { useContext } from "react";
+import RecoverPasswordContext from "@/context/RecoverPasswordContext";
+import { useRouter } from "next/navigation";
 
 export default function FormStartRecover() {
+    const router = useRouter();
+    
     const {
         register,
         handleSubmit,
+        setError,
+        reset,
         formState: { errors }
     } = useForm<EmailSchema>({
         resolver: zodResolver(emailSchema),
     });
 
+    const { verify } = useContext(RecoverPasswordContext);
+
     const sendEmail = async ({ email }: EmailSchema) => {
-        console.log(email);
+        try {
+            await verify(email);
+
+            reset();
+            router.push('/password-recover/verify-code');
+        } catch (error) {
+            setError("email", {
+                type: "manual",
+                message: "Email não cadastrado",
+            });
+        }
     }
 
 
     return (
-        <form onSubmit={handleSubmit(sendEmail)}>
-            <Form.Input
-                {...register('email')}
-                error={errors.email}
-                type="email"
-                placeholder="Insira o seu e-mail"
-            />
+        <>
+            <form onSubmit={handleSubmit(sendEmail)}>
+                <Form.Input
+                    {...register('email')}
+                    error={errors.email}
+                    type="email"
+                    placeholder="Insira o seu e-mail"
+                />
 
-            {errors.email &&
-                <Form.Span>
-                    {errors.email.message}
-                </Form.Span>
-            }
+                {errors.email &&
+                    <Form.Span>
+                        {errors.email.message}
+                    </Form.Span>
+                }
 
-            <div className="container-button">
-                <Form.ButtonSubmit>
-                    Enviar
-                </Form.ButtonSubmit>
+                <div className="container-button">
+                    <Form.ButtonSubmit>
+                        Enviar
+                    </Form.ButtonSubmit>
 
-                <Form.ButtonCancel>
-                    Cancelar
-                </Form.ButtonCancel>
-            </div>
-        </form>
+                    <Form.ButtonCancel onClick={() => router.push('/')}>
+                        Cancelar
+                    </Form.ButtonCancel>
+                </div>
+            </form>
+        </>
     )
 }
