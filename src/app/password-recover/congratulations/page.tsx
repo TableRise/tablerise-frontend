090b8550b0ce31ea8309cacdf8710100b4409916
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-27 17:13:07 -0300

    feat: create page password recover

---
+++ src/app/password-recover/congratulations/page.tsx
@@ -0,0 +1,9 @@
+import Congratulation from "@/components/password-recover/Congratulations";
+
+export default function Page() {
+    return (
+        <section>
+            <Congratulation />
+        </section>
+    )
+}
