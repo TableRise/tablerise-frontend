d92e63a6a4e1a8e12b5c0b0621cc57e5f18291a3
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-27 22:20:59 -0300

    refact: refact style and refact page

--- src/app/password-recover/template.tsx
+++ src/app/password-recover/template.tsx
@@ -5,7 +5,7 @@ import './page.css';
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-full h-screen flex justify-end bg-[#4E69A0]">
-            <div className='flex items-center justify-center w-full h-screen text-white'>
+            <div className='flex items-center justify-center w-[55.5%] h-screen text-white'>
                 <Image src={logo} alt='Logo tablerise' width={500} height={500} />
             </div>
             <div className="section-container">
