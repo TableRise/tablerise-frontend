9d331f92d053e8ccac871103d4c7048e51020e27
Amanda Portela <mandaptela@gmail.com>
2024-08-28 22:26:41 -0300

    fix: lint

--- src/components/login/input.tsx
+++ src/components/login/input.tsx
@@ -1,26 +1,21 @@
-import { input } from "@/types/login/types";
+import { input } from '@/types/login/types';
 
-export default function Input({
-    type,
-    name,
-    placeholder,
-    onChange,
-    id,
-    }: input) {
+export default function Input({ type, name, placeholder, onChange, id }: input) {
     const MAX_LENGTH_EMAIL = 30;
     const MAX_LENGTH_PASS = 50;
 
     return (
         <div>
-            <label htmlFor={type}>{name}
+            <label htmlFor={type}>
+                {name}
                 <input
                     id={id}
                     type={type}
-                    maxLength={type === 'email' ? MAX_LENGTH_EMAIL : MAX_LENGTH_PASS }
+                    maxLength={type === 'email' ? MAX_LENGTH_EMAIL : MAX_LENGTH_PASS}
                     placeholder={placeholder}
                     onChange={({ target }) => onChange(target.value)}
                 />
             </label>
         </div>
-    )
+    );
 }
