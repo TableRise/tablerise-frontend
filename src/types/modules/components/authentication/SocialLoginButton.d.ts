a12a8a48f6dad728f4446c4ac185436842c091e5
RasecMH <rasecmh0@gmail.com>
2024-09-14 16:26:50 -0300

    fix: remove errors from social login types

--- src/types/modules/components/authentication/SocialLoginButton.d.ts
+++ src/types/modules/components/authentication/SocialLoginButton.d.ts
@@ -3,5 +3,4 @@ import React from 'react';
 export type SocialLoginButtonProps = {
     title: string;
     socialType: 'discord' | 'google';
-    setError: React.Dispatch<React.SetStateAction<booblean>>;
 };
