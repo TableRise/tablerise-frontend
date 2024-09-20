2a375767747de8949a26f5cfd1d86379f3a9b78d
Amanda Portela <mandaptela@gmail.com>
2024-09-19 21:39:46 -0300

    feat: received login form's header

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,24 @@
+import SideImage from '@/components/authentication/SideImage';
 import LoginForm from '@/components/login/LoginForm';
 import SocialLogin from '@/components/login/SocialLogin';
+import Link from 'next/link';
 
 export default function Login() {
     return (
-        <div>
+        <section>
+            <div id="form-title">
+                <h2>Entrar</h2>
+                <h3>
+                    Não possui uma conta? <Link href={'/register'}>Criar conta!</Link>
+                </h3>
+            </div>
+            <SideImage />
             <LoginForm />
             <div>
                 <hr />
                 <span>ou continue com</span>
                 <SocialLogin />
             </div>
-        </div>
+        </section>
     );
 }
