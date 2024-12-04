7eeea69c14f7b55e37aa6f1c62bed2a04b432159
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-12-04 11:34:52 -0300

    feat: add forms in pages

---
+++ src/app/password-recover/secret-question/page.tsx
@@ -0,0 +1,9 @@
+import SecretQuestion from "@/components/password-recover/SecretQuestion";
+
+export default function Question() {
+    return (
+        <section>
+            <SecretQuestion />
+        </section>
+    )
+}
