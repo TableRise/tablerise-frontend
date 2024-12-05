e64d3d3acac818472cfb18cd45c501d2af5c4047
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-12-05 12:59:44 -0300

    feat: create user endpoints

---
+++ src/server/users/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const apiUser = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_USERS,
+    headers: {  
+        [`${process.env.NEXT_PUBLIC_TYPE_KEY}`]: process.env.NEXT_PUBLIC_API_ACCESS_KEY, // Acessa a variável do .env
+    }
+});
