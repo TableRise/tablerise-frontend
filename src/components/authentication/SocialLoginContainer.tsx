1f97b4e9d0cd5238d9ad09f8349f8041c57e8db2
RasecMH <rasecmh0@gmail.com>
2024-12-10 01:30:17 -0300

    feat: get error from url param and handler

--- src/components/authentication/SocialLoginContainer.tsx
+++ src/components/authentication/SocialLoginContainer.tsx
@@ -1,13 +1,30 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SocialLoginButton from './SocialLoginButton';
 import '@/components/authentication/styles/SocialLoginContainer.css';
+import { useSearchParams } from 'next/navigation';
+import errorHandler from '../../utils/errorHandler';
+import InputErrorMessage from './inputErrorMessage';
 
 export default function SocialLoginContainer(): JSX.Element {
+    const searchParams = useSearchParams();
+    const error = searchParams.get('error')
+    const [hasError, setHasError] = useState<string | undefined>()
+
+    useEffect(() => {
+        if (error) {
+            const errorList = errorHandler({ errorMessage: error })
+            setHasError(errorList[0].message);
+        }
+    }, [])
+    
     return (
-        <div className="buttons-container">
-            <SocialLoginButton title="Discord" socialType={'discord'} />
-            <SocialLoginButton title="Google" socialType={'google'} />
+        <div className="social-container">
+            <div className="buttons-container">
+                <SocialLoginButton title="Discord" socialType={'discord'} />
+                <SocialLoginButton title="Google" socialType={'google'} />
+            </div>
+            {hasError && <InputErrorMessage errorMessage={hasError} />}
         </div>
     );
 }
