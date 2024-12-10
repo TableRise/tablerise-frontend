9a18eda8eb333dccbd2cadf1aca5b7920c374f1a
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-12-10 10:47:57 -0300

    feat: create pages for recover password

---
+++ src/app/password-recover/verify-code/page.tsx
@@ -0,0 +1,9 @@
+import TwoFactor from '@/components/password-recover/TwoFactor';
+
+export default function VerifyCode() {
+    return (
+        <section>
+            <TwoFactor />
+        </section>
+    );
+}
