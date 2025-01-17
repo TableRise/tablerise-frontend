f1f72f56a31e7032f9573331d755097b48d000d1
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2025-01-17 11:48:15 -0300

    fix: prettier

--- src/components/password-recover/forms/schemas/form-new-password-schema.ts
+++ src/components/password-recover/forms/schemas/form-new-password-schema.ts
@@ -2,11 +2,10 @@ import { z } from 'zod';
 
 export const newPasswordSchema = z
     .object({
-        newPassword: z
-            .string()
-            .regex(/^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*\d).{8,32}$/, {
-                message: 'Use entre 8 e 32 caracteres, incluindo pelo menos uma letra maiúscula, um número e um dos símbolos: !@#$&*',
-            }),
+        newPassword: z.string().regex(/^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*\d).{8,32}$/, {
+            message:
+                'Use entre 8 e 32 caracteres, incluindo pelo menos uma letra maiúscula, um número e um dos símbolos: !@#$&*',
+        }),
         confirmPassword: z.string().min(1, { message: 'O campo não pode estar vazio.' }),
     })
     .refine((data) => data.confirmPassword === data.newPassword, {
