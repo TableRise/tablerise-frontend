819de6ac9d9159320cc2c5a4e9a9e074d8b6892b
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2025-01-25 12:40:45 -0300

    feat: made adjustments

--- src/app/password-recover/template.tsx
+++ src/app/password-recover/template.tsx
@@ -3,10 +3,10 @@ import './page.css';
 
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-full h-screen flex justify-end bg-[#4E69A0]">
-            <div className="flex items-center justify-center w-[55.5%] h-screen text-white">
+        <div className="w-[70%] h-screen flex justify-center bg-[#4E69A0]">
+            <div className="flex w-[28.8rem] h-screen text-white">
                 <Logo
-                    className="w-[70%]"
+                    className="w-full"
                     viewBox="-10 -15 170 80"
                     width="100%"
                     height="100%"
