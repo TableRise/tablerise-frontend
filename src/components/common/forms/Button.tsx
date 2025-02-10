5d9a0424664d80797e606e8b4e8961c31cf2c856
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-02-10 20:30:48 -0300

    refact: refact button

--- src/components/common/forms/Button.tsx
+++ src/components/common/forms/Button.tsx
@@ -1,18 +1,23 @@
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    buttonStyle: string;
+    props: string;
+}
+
 export default function Button({
     buttonStyle,
     props,
     title,
     name,
     id = name,
-    action,
     type,
     disabled = false,
-}: any): JSX.Element {
+}: ButtonProps): JSX.Element {
     return (
         <button
             name={name}
             id={id}
-            onClick={(event) => action(event)}
             disabled={disabled}
             className={`${buttonStyle} ${props}`}
             type={type}
