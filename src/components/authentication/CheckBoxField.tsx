9625b67388dc00d1cfae2f8dae3bd0b1657706ca
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-02-11 08:17:27 -0300

    refact: checkbox field

--- src/components/authentication/CheckBoxField.tsx
+++ src/components/authentication/CheckBoxField.tsx
@@ -2,27 +2,23 @@ import Link from 'next/link';
 import React from 'react';
 import '@/components/authentication/styles/CheckBoxField.css';
 import { CheckBoxFieldProps } from '@/types/modules/components/authentication/CheckBoxField';
-import { verifyError } from '@/utils/errorHandler';
 import InputErrorMessage from './inputErrorMessage';
 
 export default function CheckBoxField({
     label,
     labelWithLink,
     srcLink,
-    onChangeState,
-    inputValue,
-    errorId,
-    errorList = [],
+    name,
+    register,
+    error,
 }: CheckBoxFieldProps): JSX.Element {
-    const hasError = verifyError(errorList, errorId);
     return (
         <div className="checkbox-container">
             <label className="checkbox-label" htmlFor="checkBtn">
                 <input
                     className="input-checkbox checkbox-default checked:checkbox-icon"
                     type="checkbox"
-                    onChange={({ target: { checked } }) => onChangeState(checked)}
-                    checked={inputValue}
+                    {...register(name)}
                     id="checkBtn"
                 />
                 <h3 className="checkbox-text font-XS-regular">
@@ -38,7 +34,7 @@ export default function CheckBoxField({
                     )}
                 </h3>
             </label>
-            {hasError && <InputErrorMessage errorMessage={hasError.message} />}
+            {error && <InputErrorMessage errorMessage={error.message} />}
         </div>
     );
 }
