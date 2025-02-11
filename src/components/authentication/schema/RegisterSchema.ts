c05ebc9b5669aa5d2395eb94e63476fbc76b1dc9
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-02-11 08:12:17 -0300

    run lint

--- src/components/authentication/schema/RegisterSchema.ts
+++ src/components/authentication/schema/RegisterSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const registerZodSchema = z.object({
-    nickname: z.string().min(3, { message: "Nome do usuario não informado*" }),
+    nickname: z.string().min(3, { message: 'Nome do usuario não informado*' }),
     email: z.string().email({ message: 'Endereço de e-mail inválido*' }),
     password: z.string().regex(/^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*\d).{8,32}$/, {
         message:
@@ -12,10 +12,10 @@ const registerZodSchema = z.object({
         message: 'Você deve aceitar os termos para continuar',
     }),
 })
-.refine((data) => data.confirmPassword === data.password, {
-    message: 'Esses campos não coincidem*',
-    path: ['confirmPassword'],
-});
+    .refine((data) => data.confirmPassword === data.password, {
+        message: 'Esses campos não coincidem*',
+        path: ['confirmPassword'],
+    });
 
 export type RegisterPayload = z.infer<typeof registerZodSchema>;
 export default registerZodSchema;
