3159f65111e1dae05f5b4624ded45f576308a5de
RasecMH <rasecmh0@gmail.com>
2025-02-13 07:48:53 -0300

    feat: Create login redirect page

---
+++ src/app/login-redirect/page.tsx
@@ -0,0 +1,21 @@
+import './styles/page.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function LoginRedirect(): JSX.Element {
+    const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const { userData } = router.query;
+
+      if (userData) {
+        localStorage.setItem("userLogged", String(userData));
+      }
+
+      router.replace("/");
+    }
+  }, [router.query]);
+
+  return <></>;
+}
