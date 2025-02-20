3b015bb10cc179d413e6573d41c680821de7e7ed
RasecMH <rasecmh0@gmail.com>
2025-02-20 05:36:01 -0300

    fix: move logic to separeted component

--- src/app/login-redirect/page.tsx
+++ src/app/login-redirect/page.tsx
@@ -1,26 +1,12 @@
 'use client';
+import LoginRedirectParams from '@/components/authentication/LoginRedirectParams';
 import './styles/page.css';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 
 export default function LoginRedirect(): JSX.Element {
-    const searchParams = useSearchParams();
-    const router = useRouter();
-    const userData = searchParams.get('userData');
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            if (userData) {
-                localStorage.setItem('userLogged', String(userData));
-            }
-
-            router.replace('/');
-        }
-    }, [userData]);
-
     return (
         <Suspense>
-            <p>...</p>;
+            <LoginRedirectParams />
         </Suspense>
     );
 }
---
+++ src/components/authentication/LoginRedirectParams.tsx
@@ -0,0 +1,24 @@
+'use client';
+import './styles/page.css';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function LoginRedirectParams(): JSX.Element {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const userData = searchParams.get('userData');
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            if (userData) {
+                localStorage.setItem('userLogged', String(userData));
+            }
+
+            router.replace('/');
+        }
+    }, [userData]);
+
+    return (
+        <p>...</p>
+    );
+}
