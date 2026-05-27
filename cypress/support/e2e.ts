73497f2ef62ae82c01a3bc6adb851b85724061de
Adson Gomes Olivera <adsongoliveira2025@gmail.com>
2026-05-27 01:35:56 -0300

    fix: e2e support 2

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -20,7 +20,9 @@ const ignoredHydrationErrorMatchers = [
     'Hydration failed because the initial UI does not match what was rendered on the server.',
     'There was an error while hydrating. Because the error happened outside of a Suspense boundary, the entire root will switch to client rendering.',
     'Minified React error #418',
+    'Minified React error #423',
     'react.dev/errors/418',
+    'react.dev/errors/423',
 ];
 
 Cypress.on('uncaught:exception', (error) => {
