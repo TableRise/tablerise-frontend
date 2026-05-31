5de51fe09d5a9ee937e9e9a14abc2db545c9772a
Adson Gomes Olivera <adsongoliveira2025@gmail.com>
2026-05-31 12:18:49 -0300

    fix: button login

--- src/components/common/SocialLoginButton.tsx
+++ src/components/common/SocialLoginButton.tsx
@@ -1,8 +1,10 @@
 'use client';
 import React from 'react';
+import { useContext } from 'react';
 import DiscordLogo from '@assets/icons/social-midia/discord.svg';
 import GoogleLogo from '@assets/icons/social-midia/google.svg';
 import { SocialLoginButtonProps } from '@/types/modules/components/register/SocialLoginButton';
+import TableriseContext from '@/context/TableriseContext';
 import Link from 'next/link';
 import './styles/SocialLoginButton.css';
 
@@ -10,6 +12,7 @@ export default function SocialLoginButton({
     title,
     socialType,
 }: SocialLoginButtonProps): JSX.Element {
+    const { userLoggedToggle } = useContext(TableriseContext);
     const oauthHref = `${process.env.API_OAUTH ?? ''}/${socialType}`;
 
     return (
@@ -17,6 +20,12 @@ export default function SocialLoginButton({
             <Link
                 href={oauthHref}
                 className="social-button social-button-link button-M-outline-il"
+                onClick={(event) => {
+                    if (userLoggedToggle !== 1) return;
+
+                    event.preventDefault();
+                    window.location.replace('/');
+                }}
             >
                 {socialType === 'discord' && <DiscordLogo className="social-icon" />}
                 {socialType === 'google' && <GoogleLogo className="social-icon" />}
--- src/server/users/login.ts
+++ src/server/users/login.ts
@@ -15,12 +15,22 @@ export const postLogin = async (payload: LoginPayload): Promise<AxiosResponse | 
 
         return result;
     } catch ({ response }: AxiosError | any) {
-        if (response.status == 404) throw new Error('*Usuario não cadastrado');
-        if (response.status == 401)
+        if (!response) {
+            throw new Error('*Conexão falhou, tente novamente mais tarde.');
+        }
+
+        if (response.status === 404) {
+            throw new Error('*Usuario não cadastrado');
+        }
+
+        if (response.status === 401) {
             throw new Error('*Dados de email ou senha incorretos. Tente novamente.');
-        if (response.status == 400)
+        }
+
+        if (response.status === 400) {
             throw new Error('*Usuário com status incorreto para login');
-        if (response.status !== 200)
-            throw new Error('*Algo deu errado. Tente novamente.');
+        }
+
+        throw new Error('*Algo deu errado. Tente novamente.');
     }
 };
